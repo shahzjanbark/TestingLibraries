b8655d6e2fd514cc5942bf7f66c0dbdff7fd51e5
JB-Shahzaib <shahzaibjanbark@gmail.com>
2023-10-20 11:31:45 +0500

    second commit

--- MathLibrary/build.gradle.kts
+++ MathLibrary/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -32,6 +33,10 @@ android {
     }
 }
 
+
+
+
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -40,4 +45,19 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+
+publishing{
+    publications{
+        register<MavenPublication>("release"){
+            groupId = "com.test.mathslibrary"
+            artifactId = "mathUtils"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
